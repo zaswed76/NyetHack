8f0a7b888305549b52859bebe4b0d472c42ebffa
sergdell <zaswed76@gmail.com>
2020-03-14 06:25:42 +0200

    add main

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,19 @@
-fun main() {
+
+
+
+fun main(args: Array<String>) {
+   val name = "Madrigal"
+   var healthPoints = 89
+   val isBlessed = true
+   val isImmortal = false
+
+   val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+   if (auraVisible) {
+      println("GREEN")
+   } else {
+      println("NONE")
+   }
 
 }
+
+
